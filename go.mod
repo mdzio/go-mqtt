5020cffbac5aae96bbd15bbb7ca3df123357db61
mdzio <info@ccu-historian.de>
2022-05-23 18:57:49 +0200

    add client test, update dependencies

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/mdzio/go-mqtt
 go 1.15
 
 require (
-	github.com/gorilla/websocket v1.4.2
+	github.com/gorilla/websocket v1.5.0
+	github.com/mdzio/go-lib v0.1.7
 	github.com/mdzio/go-logging v1.0.0
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.1
 )
--- service/client_test.go
+++ service/client_test.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"strconv"
 	"testing"
 
+	"github.com/mdzio/go-lib/testutil"
 	"github.com/mdzio/go-mqtt/message"
 )
 
@@ -17,12 +19,88 @@ func TestClient(t *testing.T) {
 	msg.SetKeepAlive(10)
 	msg.SetWillTopic([]byte("will"))
 	msg.SetWillMessage([]byte("send me home"))
-	msg.SetUsername([]byte(""))
-	msg.SetPassword([]byte(""))
+	msg.SetUsername([]byte(testutil.Config(t, "MQTT_SERVER_USER")))
+	msg.SetPassword([]byte(testutil.Config(t, "MQTT_SERVER_PASSWD")))
 
-	err := c.Connect("tcp://localhost:1883", msg)
+	err := c.Connect(testutil.Config(t, "MQTT_SERVER_ADDR"), msg)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer c.Disconnect()
+	log.Info("Connected")
+
+	submsg := message.NewSubscribeMessage()
+	if err := submsg.AddTopic([]byte("#"), 0); err != nil {
+		t.Fatalf("Adding remote topic failed: %v", err)
+	}
+
+	onCompleteCalled := make(chan struct{})
+	var onComplete OnCompleteFunc = func(msg, ack message.Message, err error) error {
+		defer func() {
+			close(onCompleteCalled)
+		}()
+		if err != nil {
+			log.Errorf("Subscribing remote topic failed: %v", err)
+		} else {
+			log.Info("Subscribing remote topic succeeded")
+		}
+		return nil
+	}
+
+	onPublishCalled := make(chan struct{})
+	var onPublish OnPublishFunc = func(pubmsg *message.PublishMessage) error {
+		defer func() {
+			select {
+			case onPublishCalled <- struct{}{}:
+			default:
+			}
+		}()
+		rt := string(pubmsg.Topic())
+		log.Infof("Incoming message on remote topic %s, retain %t with payload %s", rt, pubmsg.Retain(), string(pubmsg.Payload()))
+		return nil
+	}
+	if err := c.Subscribe(submsg, onComplete, onPublish); err != nil {
+		t.Fatalf("Subscribing remote topic failed: %v", err)
+	}
+
+	<-onCompleteCalled
+	<-onPublishCalled
+}
+
+func TestClient2(t *testing.T) {
+	c := new(Client)
+
+	msg := message.NewConnectMessage()
+	msg.SetVersion(byte(4))
+	msg.SetCleanSession(true)
+	msg.SetClientID([]byte("test client"))
+	msg.SetKeepAlive(10)
+	msg.SetUsername([]byte(testutil.Config(t, "MQTT_SERVER_USER")))
+	msg.SetPassword([]byte(testutil.Config(t, "MQTT_SERVER_PASSWD")))
+
+	err := c.Connect(testutil.Config(t, "MQTT_SERVER_ADDR"), msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Disconnect()
+	log.Info("Connected")
+
+	pubmsg := message.NewPublishMessage()
+	pubmsg.SetTopic([]byte("test-topic"))
+	pubmsg.SetQoS(0)
+
+	for cnt := 10; cnt >= 0; cnt-- {
+		var onComplete OnCompleteFunc = func(msg, ack message.Message, err error) error {
+			if err != nil {
+				log.Errorf("OnComplete: Publishing failed: %v", err)
+			} else {
+				log.Trace("OnComplete: Publishing succeeded")
+			}
+			return nil
+		}
+		pubmsg.SetPayload([]byte(strconv.Itoa(cnt)))
+		if err := c.Publish(pubmsg, onComplete); err != nil {
+			t.Fatalf("Publishing failed: %v", err)
+		}
+	}
 }
